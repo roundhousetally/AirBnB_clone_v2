2bb9b7a363a90e0bb8180591e716a2f69280bcfa
Roundhousetally <tahlia.w.roper@gmail.com>
2020-08-17 19:48:36 +0000

    pep8 tested

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,12 +3,14 @@
 from fabric.api import *
 from datetime import datetime
 
+
 def do_pack():
     """ packs the webstatic """
     mktime = datetime.now().strftime("Y%m%d%H%M%S")
     local('mkdir -p versions')
     try:
-        archfile = local('sudo tar -czvf versions/web_static_{}.tgz web_static'.format(mktime))
+        archfile = local('sudo tar -czvf versions/web_static_{}.tgz web_static'
+                         .format(mktime))
         return(archfile)
     except BaseException:
         return None
