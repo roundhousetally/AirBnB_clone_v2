49a5817163e786fb270c4983683ef924d17777be
Roundhousetally <tahlia.w.roper@gmail.com>
2020-08-17 19:43:15 +0000

    task 1 cannot run

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this script generates a compressed archive file """
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    """ packs the webstatic """
+    mktime = datetime.now().strftime("Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    try:
+        archfile = local('sudo tar -czvf versions/web_static_{}.tgz web_static'.format(mktime))
+        return(archfile)
+    except BaseException:
+        return None
