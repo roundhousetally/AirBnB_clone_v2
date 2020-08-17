a13adc5d64ffb7f7352ae4b601175a9e726b5299
Roundhousetally <tahlia.w.roper@gmail.com>
2020-08-17 18:18:23 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,14 @@
 #!/usr/bin/env bash
-# Task 0
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-sudo mkdir /data
-sudo mkdir /data/web_static
-sudo mkdir /data/web_static/releases
-sudo mkdir /data/web_static/shared
-sudo mkdir /data/web_static/releases/test
-
-indx="<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
-
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "$indx" | sudo tee /data/web_static/releases/test/index.html
-
-if [ ! -L /data/web_static/current ]; then
-        sudo rm /data/web_static/current
-        echo "in if"
-fi
-sudo ln -s /data/web_static/releases/test /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data
-
-srvr="^server {$"
-repl="server {\
-\n\tlocation /hbnv_static {\
-\n\t\talias /data/web_static/current/;\
-\n\t\tinternal;\
-\n\t}\n"
-file="/etc/nginx/sites-available/default"
-
-sudo sed -i 's,'"$srvr"','"$repl"',' "$file"
-
+# sets up web servers to deploy airbnb clone
+apt-get update
+apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared    
+ufw allow 'Nginx HTTP'
+sudo touch /data/web_static/releases/test/index.html 
+echo "<h1>Fake Content as a test</h1>" | sudo tee /data/web_static/releases/test/index.html 
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i "45a \\\tlocation /hbnb_static {\n" /etc/nginx/sites-available/default
+sed -i "46a \\\t\talias /data/web_static/current/;\n" /etc/nginx/sites-available/default
+sed -i "47a \\\t}\n" /etc/nginx/sites-available/default 
 sudo service nginx restart
