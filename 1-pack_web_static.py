5003ea9ce4a7f0a4e1badd786ade91ba4c4fb0ef
Roundhousetally <tahlia.w.roper@gmail.com>
2020-08-17 21:53:22 +0000

    realized an percent sign missing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def do_pack():
     """ packs the webstatic """
-    mktime = datetime.now().strftime("Y%m%d%H%M%S")
+    mktime = datetime.now().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
     try:
         archfile = local('sudo tar -czvf versions/web_static_{}.tgz web_static'
