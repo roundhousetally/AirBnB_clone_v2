e54747fe524461e240321aa8b7147a5356c47934
Roundhousetally <tahlia.w.roper@gmail.com>
2020-08-31 20:56:04 +0000

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" starts a flask web app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """ returns hello hb """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ displays hbnb """
+    return "HBHB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ displays C and text """
+    txt = text.replace("_", " ")
+    return "C {}".format(txt)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py(text="is cool"):
+    """ route """
+    txt = text.replace("_", " ")
+    return "Python {}".format(txt)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
