688b3018b57a4946944e9a7aea4f9c73963590cb
Roundhousetally <tahlia.w.roper@gmail.com>
2020-09-01 17:10:12 +0000

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,16 +9,19 @@ def home():
     """ returns hello hb """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ displays hbnb """
-    return "HBHB"
+    return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """ displays C and text """
     txt = text.replace("_", " ")
     return "C {}".format(txt)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
