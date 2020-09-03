18256b65a79263eeab8de73c0e86c334e2ddd3c5
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-03 02:47:33 +0000

    not finished just sleepy

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" starts a flask web app """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def statesid():
+    """ displays html page """
+    allstates = storage.all(State)
+    if id is None:
+        return render_template("9-states.html", allstates=allstates)
+    statewid = allstates.get("State." + id)
+    return render_template("9-states.html", statewid=statewid)
+
+@app.teardown_appcontext
+def tear(exc):
+    """ remove the sqlalchemy sesh """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run()
