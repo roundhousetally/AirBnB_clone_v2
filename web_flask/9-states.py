3110deb0bbe7294ae7e79debc4282ab8866872c3
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-03 04:14:27 +0000

    9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 
 @app.route("/states", strict_slashes=False)
 @app.route("/states/<id>", strict_slashes=False)
-def statesid():
+def statesid(id=None):
     """ displays html page """
     allstates = storage.all(State)
     if id is None:
         return render_template("9-states.html", allstates=allstates)
-    statewid = allstates.get("State." + id)
+    statewid = allstates.get("State." + str(id))
     return render_template("9-states.html", statewid=statewid)
+
 
 @app.teardown_appcontext
 def tear(exc):
